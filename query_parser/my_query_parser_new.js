c0613d4996ddf9f747c260f500db091af44af5fd
bhagya <bhagyashiranakalankacrazy999@gmail.com>
2020-06-29 03:02:54 +0530

    parser almost done

--- query_parser/my_query_parser_new.js
+++ query_parser/my_query_parser_new.js
@@ -1,17 +1,45 @@
 const artist_charactor = "s";
-
+var fs = require("fs");
+const numberMap = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+  ten: 10,
+};
 function query_parser(text) {
   var text = text.toLowerCase();
-  var must_query = [];
-  var should_query = [];
+  var must_query = {
+    _all: [],
+    artist: [],
+    lyrics_by: [],
+    music: [],
+    duration: [],
+    rating: [],
+  };
+  var should_query = {
+    _all: [],
+    artist: [],
+    lyrics_by: [],
+    music: [],
+    duration: [],
+    rating: [],
+  };
   var must_not_query = {};
   var range_query = {};
   var top_query = {};
   var phase_query = [];
+  var sort_query = [];
   const wordList = text.split(` `);
 
   var i = 0;
   var andWords = [];
+
   while (i < wordList.length) {
     if (wordList[i] === "and") {
       //handle previous word
@@ -20,30 +48,33 @@ function query_parser(text) {
       andWords.push(wordList[i]);
       if (previousWord[previousWord.length - 1] === '"') {
         var [phase, andList] = findPhaseBackword(i - 1);
-        must_query.push({ _all: [phase, "match_phase"] });
+        must_query._all.push([phase, "match_phase"]);
         andWords = andWords.concat(andList);
       } else if (previousWord[previousWord.length - 1] === artist_charactor) {
-        must_query.push({
-          artist: [previousWord.substring(0, previousWord.length - 1), "term"],
-        });
+        must_query.artist.push([
+          previousWord.substring(0, previousWord.length - 1),
+          "term",
+        ]);
       } else {
-        must_query.push({ _all: [previousWord, "term"] });
+        console.log(must_query);
+        must_query._all.push([previousWord, "term"]);
       }
       //handle next word
       var nextWord = wordList[i + 1];
       andWords.push(nextWord);
       if (nextWord[0] === '"') {
         var [phase, andList] = findPhaseForword(i + 1);
-        must_query.push({ _all: [phase, "match_phase"] });
+        must_query._all.push([phase, "match_phase"]);
         andWords = andWords.concat(andList);
         i += andList.length + 1;
       } else if (nextWord[nextWord.length - 1] === artist_charactor) {
-        must_query.push({
-          artist: [nextWord.substring(0, nextWord.length - 1), "term"],
-        });
+        must_query.artist.push([
+          nextWord.substring(0, nextWord.length - 1),
+          "term",
+        ]);
         i++;
       } else {
-        must_query.push({ _all: [nextWord, "term"] });
+        must_query._all.push([nextWord, "term"]);
         i++;
       }
     }
@@ -56,21 +87,112 @@ function query_parser(text) {
       wordList.splice(index, 1);
     }
   });
-  console.log(wordList);
+
   i = 0;
   while (i < wordList.length) {
     var currentWord = wordList[i];
-    if (currentWord[0] === '"') {
+    var wordDouble = wordList[i] + " " + wordList[i + 1];
+    //top # songs handle
+    if (wordDouble === "best songs" || wordDouble === "top songs") {
+      var number = wordList[i + 2];
+      number =
+        numberMap[number] !== undefined ? numberMap[number] : parseInt(number);
+      console.log(number);
+      if (number === NaN) {
+        number = 10;
+      }
+      sort_query.push({ rating: number });
+
+      i += 3;
+    } //between | less than | greater than mins
+    else if (currentWord === "minute") {
+      if (wordList[i + 2] == "greater") {
+        var number_g = wordList[i + 1];
+        if (number_g[number_g.length - 1] === "t") {
+          number_g = number_g.substring(0, number_g.length - 1);
+          number_g =
+            numberMap[number_g] === undefined
+              ? parseInt(number_g)
+              : numberMap[number_g];
+        } else {
+          number_g =
+            numberMap[number_g] === undefined
+              ? parseInt(number_g)
+              : numberMap[number_g];
+        }
+        must_query.duration.push([{ gte: number_g }, "range"]);
+        if (wordList[i + 3] === "sindu") {
+          i++;
+        }
+        i += 3;
+      } else if (wordList[i + 2] === "lesser") {
+        var number_l = wordList[i + 1];
+        if (number_l[number_l.length - 1] === "t") {
+          number_l = number_l.substring(0, number_l.length - 1);
+          number_l =
+            numberMap[number_l] === undefined
+              ? parseInt(number_l)
+              : numberMap[number_l];
+        } else {
+          number_l =
+            numberMap[number_l] === undefined
+              ? parseInt(number_l)
+              : numberMap[number_l];
+        }
+        must_query.duration.push([{ lte: number_l }, "range"]);
+        if (wordList[i + 3] === "sindu") {
+          i++;
+        }
+        i += 3;
+      } else if (wordList[i + 3] === "between") {
+        var number_l = wordList[i + 1];
+        var number_g = wordList[i + 2];
+
+        if (number_l[number_l.length - 1] === "x") {
+          console.log("number_l", number_l);
+          number_l = number_l.substring(0, number_l.length - 1);
+          number_l =
+            numberMap[number_l] === undefined
+              ? parseInt(number_l)
+              : numberMap[number_l];
+        } else {
+          number_l =
+            numberMap[number_l] === undefined
+              ? parseInt(number_l)
+              : numberMap[number_l];
+        }
+        if (number_g[number_g.length - 1] === "x") {
+          number_g = number_g.substring(0, number_g.length - 1);
+          number_g =
+            numberMap[number_g] === undefined
+              ? parseInt(number_g)
+              : numberMap[number_g];
+        } else {
+          number_g =
+            numberMap[number_g] === undefined
+              ? parseInt(number_g)
+              : numberMap[number_g];
+        }
+        must_query.duration.push([{ gte: number_l, lte: number_g }, "range"]);
+        if (wordList[i + 4] === "sindu") {
+          i++;
+        }
+        i += 4;
+      }
+    } // match phases handle
+    else if (currentWord[0] === '"') {
       var [phase, andList] = findPhaseForword(i);
-      should_query.push({ _all: [phase, "match_phase"] });
+      should_query._all.push([phase, "match_phase"]);
       i += andList.length + 1;
-    } else if (currentWord[currentWord.length - 1] === artist_charactor) {
-      should_query.push({
-        artist: [currentWord.substring(0, currentWord.length - 1), "term"],
-      });
+    } // normal term handle
+    else if (currentWord[currentWord.length - 1] === artist_charactor) {
+      should_queryartist.push([
+        currentWord.substring(0, currentWord.length - 1),
+        "term",
+      ]);
       i++;
     } else {
-      should_query.push({ _all: [currentWord, "term"] });
+      should_query._all.push([currentWord, "term"]);
       i++;
     }
   }
@@ -112,9 +234,103 @@ function query_parser(text) {
     return [phase, andList];
   }
 
-  console.log(must_query);
-  console.log(should_query);
+  return [must_query, should_query, sort_query];
 }
 
-var sample_text = '1s 2 "3s df" and 4 gf "5s gh" and "6s ff" "fdf dsdsd" jjk';
-query_parser(sample_text);
+var sample_text = "amaradewas and latha minute 180 350t between top songs 10";
+
+t1 = Date.now();
+const [a, b, c] = query_parser(sample_text);
+t2 = Date.now();
+console.log(t2 - t1);
+
+function query_generator(must_query, should_query, sort_query) {
+  var query = {};
+  var is_query_started = false;
+  if (sort_query.length > 0) {
+    console.log(sort_query[0].rating);
+    query["size"] = sort_query[0].rating;
+    query["sort"] = [{ rating: "desc", _score: "desc" }];
+  }
+  query["query"] = { bool: {} };
+  //handle must query
+  var is_must_started = false;
+  for (key in must_query) {
+    if (must_query[key].length !== 0) {
+      if (!is_must_started) {
+        query.query.bool["must"] = [];
+        is_must_started = true;
+      }
+
+      for (valIndex = 0; valIndex < must_query[key].length; valIndex++) {
+        switch (
+          must_query[key][valIndex][must_query[key][valIndex].length - 1]
+        ) {
+          case "term":
+            var temp_q_m = { match: {} };
+            temp_q_m.match[key] = { query: must_query[key][valIndex][0] };
+            query.query.bool.must.push(temp_q_m);
+            break;
+          case "match_phase":
+            var temp_q_mp = { match_phase: {} };
+            temp_q_mp.match_phase[key] = {
+              query: must_query[key][valIndex][0],
+            };
+            query.query.bool.must.push(temp_q_mp);
+            break;
+          case "range":
+            var temp_q_r = { range: { duration: {} } };
+            if (must_query[key][valIndex][0].gte !== undefined) {
+              temp_q_r.range.duration["gte"] = must_query[key][valIndex][0].gte;
+            }
+            if (must_query[key][valIndex][0].lte !== undefined) {
+              temp_q_r.range["duration"]["lte"] =
+                must_query[key][valIndex][0].lte;
+            }
+            query.query.bool.must.push(temp_q_r);
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+  var is_should_started = false;
+  for (key in should_query) {
+    if (should_query[key].length !== 0) {
+      if (!is_should_started) {
+        query.query.bool["should"] = [];
+        is_should_started = true;
+      }
+
+      for (valIndex = 0; valIndex < should_query[key].length; valIndex++) {
+        switch (
+          should_query[key][valIndex][should_query[key][valIndex].length - 1]
+        ) {
+          case "term":
+            var temp_q_m = { match: {} };
+            temp_q_m.match[key] = { query: should_query[key][valIndex][0] };
+            query.query.bool.should.push(temp_q_m);
+            break;
+          case "match_phase":
+            var temp_q_mp = { match_phase: {} };
+            temp_q_mp.match_phase[key] = {
+              query: should_query[key][valIndex][0],
+            };
+            query.query.bool.should.push(temp_q_mp);
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+
+  return query;
+}
+
+q = query_generator(a, b, c);
+q = JSON.stringify(q);
+fs.writeFile("query.json", q, (err) => {
+  if (err) console.log(err);
+});
--- query_parser/test.js
+++ query_parser/test.js
@@ -1,2 +1,4 @@
-var a = "tssfdf s ";
-console.log(a.trim(`s`));
+a = "f10j0n";
+a = parseInt(a);
+console.log(a);
+console.log(typeof a == "number");
